bb6ace6fe6190d0155fd45d94f1d02d79d70faa4
imadcollin <imad.collin@gmail.com>
2019-09-21 15:42:25 +0200

    Add pricing cards

---
+++ src/app/price-cards/price-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-cards',
+  templateUrl: './price-cards.component.html',
+  styleUrls: ['./price-cards.component.css']
+})
+export class PriceCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
