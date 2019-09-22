5f8189f11827af7658f98cce1b3695790c9d24f2
imadcollin <imad.collin@gmail.com>
2019-09-22 22:28:36 +0200

    Add page not found

--- src/app/routerConfig.ts
+++ src/app/routerConfig.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ApiComponent } from './api/api.component';
 import { WordDetailsComponent } from './word-details/word-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const appRoutes: Routes = [
         {
-      path: 'header',
-      component: HeaderComponent
+      path: '',
+      component: ApiComponent
     } ,
         {
       path: 'footer',
@@ -20,6 +21,10 @@ export const appRoutes: Routes = [
         {
       path: 'detail',
       component: WordDetailsComponent
+    } ,
+        {
+      path: '**',
+      component:PageNotFoundComponent
     } 
   ];
 
